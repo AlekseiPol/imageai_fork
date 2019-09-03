53c65749ba26953e1bfc2cf88a2850d0c07eec12
AlekseiPol <52461183+AlekseiPol@users.noreply.github.com>
2019-09-03 21:29:52 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup,find_packages
 setup(name="imageai",
       version='2.1.4',
       description='A python library built to empower developers to build applications and systems  with self-contained Computer Vision capabilities',
-      url="https://github.com/OlafenwaMoses/ImageAI",
+      url="https://github.com/AlekseiPol/imageai_fork",
       author='Moses Olafenwa and John Olafenwa',
       author_email='guymodscientist@gmail.com',
       license='MIT',
